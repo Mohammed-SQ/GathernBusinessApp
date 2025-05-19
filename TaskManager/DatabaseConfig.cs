703bbfd28bd74e86d2281472485b7b084ee8ab44
ALLUQMAN, MUHAMMED SUFYAN MUHAMMED <202210391@dcc.kfupm.edu.sa>
2025-05-19 21:56:15 +0300

    Delete TaskManager directory

--- TaskManager/DatabaseConfig.cs
+++
@@ -1,4 +0,0 @@
-public static class DatabaseConfig
-{
-    public static string ConnectionString => @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\ll\source\repos\testing\testing\App_Data\StudyBuddyDB.mdf;Integrated Security=True";
-}
