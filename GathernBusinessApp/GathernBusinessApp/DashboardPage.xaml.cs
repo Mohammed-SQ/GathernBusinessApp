7248d555942be390e6e3aa308adbe717be5ed691
ALLQUMAN, MUHAMMED SUFYAN MUHAMMED <202210391@dcc.kfupm.edu.sa>
2025-05-24 21:24:44 +0300

    c

--- GathernBusinessApp/GathernBusinessApp/DashboardPage.xaml.cs
+++ GathernBusinessApp/GathernBusinessApp/DashboardPage.xaml.cs
@@ -1,3 +1,4 @@
+﻿using System;
 using Microsoft.Maui.Controls;
 
 namespace GathernBusinessApp;
@@ -9,5 +10,41 @@ public partial class DashboardPage : ContentPage
         InitializeComponent();
     }
 
-    // Add your event handlers and logic here
+    private async void OnNotificationClicked(object sender, EventArgs e)
+    {
+        // TODO: Navigate or show notifications page
+        await DisplayAlert("إشعار", "تم النقر على أيقونة الإشعارات", "حسناً");
+    }
+
+    private async void OnChatClicked(object sender, EventArgs e)
+    {
+        // TODO: Navigate to chat page
+        await DisplayAlert("دردشة", "تم النقر على أيقونة الدردشة", "حسناً");
+    }
+
+    private async void OnSupportClicked(object sender, EventArgs e)
+    {
+        // TODO: Navigate to support page
+        await DisplayAlert("الدعم", "تم النقر على أيقونة الدعم", "حسناً");
+    }
+
+    private async void OnCalendarClicked(object sender, EventArgs e)
+    {
+        await Shell.Current.GoToAsync("//CalendarPage");
+    }
+
+    private async void OnBookingClicked(object sender, EventArgs e)
+    {
+        await Shell.Current.GoToAsync("//BookingPage");
+    }
+
+    private async void OnPropertyClicked(object sender, EventArgs e)
+    {
+        await Shell.Current.GoToAsync("//PropertyPage");
+    }
+
+    private async void OnMoreClicked(object sender, EventArgs e)
+    {
+        await Shell.Current.GoToAsync("//ExtraPage");
+    }
 }
